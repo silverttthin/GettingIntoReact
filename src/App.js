5a890106613f9703f1fbee974cc4478ff194ed6f
root <silverttthin@naver.com>
2022-08-02 09:06:58 +0000

    setState()

--- src/App.js
+++ src/App.js
@@ -2,8 +2,26 @@ import React from 'react';
 
 class App extends React.Component{
 
+  state = {
+    count : 0,
+  };
+
+  plus = () => this.setState(current => ({
+    count: current.count + 1,
+  }))
+  
+  minus = () => this.setState(current => ({
+    count: current.count - 1,
+  }))
+
   render(){
-    return <h1>This is Class component.</h1>
+    return (
+      <div>
+        <h1>The number is : {this.state.count}</h1>
+        <button onClick={this.plus}>PLUS</button>
+        <button onClick={this.minus}>MINUS</button>
+      </div>
+    );
   }
 
 }
