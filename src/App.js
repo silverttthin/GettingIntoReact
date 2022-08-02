259795dfe504f81e3871e7eef4cbd3543774af2b
root <silverttthin@naver.com>
2022-08-02 10:01:47 +0000

    Initial commit in app branch

--- src/App.js
+++ src/App.js
@@ -2,46 +2,13 @@ import React from 'react';
 
 class App extends React.Component{
 
-  constructor(props){
-    super(props);
-    console.log("constructor!");
-  }
+    render(){
+        return <div />;
+    }
 
-  state = {
-    count : 0,
-  };
 
-  plus = () => this.setState(current => ({
-    count: current.count + 1,
-  }))
+
   
-  minus = () => this.setState(current => ({
-    count: current.count - 1,
-  }))
-
-  componentDidMount(){
-    console.log("component did mount!");
-  }
-
-  componentDidUpdate(){
-    console.log("component did update!");
-  }
-
-  componentWillUnmount(){
-    console.log("bye..");
-  }
-
-  render(){
-    console.log("render!");
-    return (
-      <div>
-        <h1>The number is : {this.state.count}</h1>
-        <button onClick={this.plus}>PLUS</button>
-        <button onClick={this.minus}>MINUS</button>
-      </div>
-    );
-  }
-
 }
 
 
