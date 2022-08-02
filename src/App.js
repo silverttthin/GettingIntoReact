ae569938ac360b2f40db23a78348661eb4e74fc9
root <silverttthin@naver.com>
2022-08-02 09:43:30 +0000

    four lifecycle method: constructor, componentDidMount, componentDidupdate, componentWillUnmount

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React from 'react';
 
 class App extends React.Component{
 
+  constructor(props){
+    super(props);
+    console.log("constructor!");
+  }
+
   state = {
     count : 0,
   };
@@ -14,7 +19,20 @@ class App extends React.Component{
     count: current.count - 1,
   }))
 
+  componentDidMount(){
+    console.log("component did mount!");
+  }
+
+  componentDidUpdate(){
+    console.log("component did update!");
+  }
+
+  componentWillUnmount(){
+    console.log("bye..");
+  }
+
   render(){
+    console.log("render!");
     return (
       <div>
         <h1>The number is : {this.state.count}</h1>
