dc199b96534f450db78eea9b1f488376de10b799
root <silverttthin@naver.com>
2022-08-03 02:29:03 +0000

    api and loading(-161p)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import React from 'react';
+import axios from 'axios';
+
 
 class App extends React.Component{
 
-    render(){
-        return <div />;
+    state = {
+        isLoading : true,
+        movies: [],
     }
 
+    getMovies = async () => {
+        const {
+            data: {
+                data: {movies}
+            }
+        } = await axios.get('https://yts-proxy.now.sh/list_movies.json');
 
+        this.setState({movies, isLoading : false});
 
-  
+    }
+
+    componentDidMount(){
+    // 영화 데이터 로딩!
+        this.getMovies();
+        
+    }
+
+    render(){
+        const {isLoading} = this.state;
+        return <div>{isLoading? 'Loading...' : 'We are ready'}</div>
+    }
 }
 
 
