aeefd404e31ec379f0f001372d5afe275fe84bb8
root <silverttthin@naver.com>
2022-08-04 00:47:01 +0000

    return Movie component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-
+import Movie from './Movie'
 
 class App extends React.Component{
 
@@ -27,8 +27,18 @@ class App extends React.Component{
     }
 
     render(){
-        const {isLoading} = this.state;
-        return <div>{isLoading? 'Loading...' : 'We are ready'}</div>
+        const {isLoading, movies} = this.state;
+        return <div>{isLoading? 'Loading...' : movies.map((movie) => {
+            console.log(movie);
+            return <Movie 
+            key = {movie.id}
+            id = {movie.id}
+            year = {movie.year}
+            title = {movie.title}
+            summary = {movie.summary}
+            poster = {movie.medium_cover_image}
+            />;
+        })}</div>
     }
 }
 
